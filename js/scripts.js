1c8bd1f0ad73ce9b887a74a87a230cd778ee168d
owner <owner@Thinkpad>
2018-12-03 12:40:05 -0500

    updated styling

--- js/scripts.js
+++ js/scripts.js
@@ -11,20 +11,29 @@ $('button').on('click', function () {
         $(this).parent('li').find('li').removeClass('hidden');
         $(this).parent().find('*').removeClass('selected');
         $(this).parent().find('ul').addClass('hidden');
-        $('html, body').animate({ scrollTop: $(document).height()+100 }, 'slow');
-        $(this).find('*').prop('checked',false);
+        $('html, body').animate({ scrollTop: $(document).height() + 100 }, 'slow');
+        $(this).find('*').prop('checked', false);
+        $(this).removeClass('goback');
         return false;
-
 
     } else {
         $(this).addClass('selected');
         $(this).parent('li').siblings('li').addClass('hidden');
         $(this).parent('li').children('ul').removeClass('hidden');
         $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
-        $(this).find('*').prop('checked',false);
-
-
-
+        $(this).find('*').prop('checked', false);
         return false;
     }
 })
+
+$('button').mouseenter(function () {
+    if ($(this).hasClass('selected')) {
+        $(this).addClass('goback');
+    }
+});
+
+$('button').mouseleave(function () {
+    if ($(this).hasClass('selected')) {
+        $(this).removeClass('goback');
+    }
+});
